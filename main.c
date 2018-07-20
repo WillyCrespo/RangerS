fe337ca009eebef0831ce4519c7db89bf242206f
WillyCrespo <36381521+WillyCrespoGD@users.noreply.github.com>
2018-07-20 06:50:42 -0400

    THE MAIN THING

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <3ds.h>
+
+int main()
+{
+	gfxInitDefault(); // Init graphic stuff
+
+	// We need these 2 buffers for APT_DoAppJump() later.
+	u8 param[0x300];
+	u8 hmac[0x20];
+
+	// Loop as long as the status is not exit
+	while(aptMainLoop())
+	{
+		// Scan hid shared memory for input events
+		hidScanInput();
+
+		if(hidKeysDown() & KEY_A) // If the A button got pressed, start the app launch
+		{
+			// Clear both buffers
+			memset(param, 0, sizeof(param));
+			memset(hmac, 0, sizeof(hmac));
+
+			// Prepare for the app launch
+			APT_PrepareToDoApplicationJump(0, 0x0004001000022400LL, 0); // *EUR* camera app title ID
+			// Tell APT to trigger the app launch and set the status of this app to exit
+			APT_DoApplicationJump(param, sizeof(param), hmac);
+		}
+
+		// Flush + swap framebuffers and wait for VBlank. Not really needed in this file,idk
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gspWaitForVBlank();
+	}
+
+	gfxExit();
+
+	return 0;
+}
